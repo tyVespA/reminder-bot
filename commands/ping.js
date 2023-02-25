0cabfd6100311c228cb00f05b3619e09b6bca7c3
tyVespA <migliavacca.marco08@gmail.com>
2023-02-25 14:08:24 +0100

    Add slash commands

---
+++ commands/ping.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with Pong!"),
+  async execute(interaction) {
+    await interaction.reply("Pong!");
+  },
+};
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("server")
+    .setDescription("Provides information about the server."),
+  async execute(interaction) {
+    // interaction.guild is the object representing the Guild in which the command was run
+    await interaction.reply(
+      `This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`
+    );
+  },
+};
